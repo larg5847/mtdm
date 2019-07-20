354a02c41694e84fc2033e63d76ea08879704669
Alfredo Ruiz <spartan5847@hotmail.com>
2019-07-20 06:24:07 -0500

    creacion de pantalla principal

---
+++ JuegoConjuntov0.1/Assets/Paquetes/PaquetesAlfredo/ejeZ/scripts/cargarEscenas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cargarEscenas : MonoBehaviour
+{
+   public void cargarEscena(int numEscena)
+    {
+        SceneManager.LoadScene(numEscena);
+    }
+}
