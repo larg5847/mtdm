a69c4f62e531cbd84523877e7e1fe5632fd590ee
larg5847 <spartan5847@hotmail.com>
2019-09-20 11:12:00 -0500

    demo v0.5 tntech

---
+++ juegoConjuntov0.5/Assets/scripts/reinicioNivel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reinicioNivel : MonoBehaviour
+{
+    int numEscena;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numEscena = SceneManager.GetActiveScene().buildIndex;
+    }
+    public void cargarEscena()
+    {
+        Debug.Log("es la escena..." + numEscena);
+        SceneManager.LoadScene(numEscena);
+    }
+}
